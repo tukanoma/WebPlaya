769c181d0ba0ba1b6fe6a640ba3535b25a311a87
Kuga Ski <mail@rixray.com>
2023-06-06 11:10:12 +0800

    webvtt test

--- index.js
+++ index.js
@@ -138,41 +138,41 @@ async function generateVttFile(filename, duration) {
         console.log('\x1b[33m%s\x1b[0m', `${filename}.vtt already exists, skipping processing`);
         return;
     } catch (err) {
+        const width = 320;
+        const height = 180;
+        const interval = 1;
+        const col = 15;
+        const row = 15;
+        let thumbOutput = 'WEBVTT\n\n';
+        const startTime = moment('00:00:00', 'HH:mm:ss.SSS');
+        const endTime = moment('00:00:00', 'HH:mm:ss.SSS').add(interval, 'seconds');
+        const totalImages = Math.floor(duration / interval); // Total no of thumbnails
+        const totalSpirits = Math.ceil(duration / interval / (row * col)); // Total no of spirits
+        let newStr = filename.replace(/^\/app\/public/, "");
+        for (let k = 0; k < totalSpirits; k++) {
+            for (let i = 0; i < row; i++) {
+                for (let j = 0; j < col; j++) {
+                    const currentImageCount = k * row * col + i * col + j;
+                    if (currentImageCount > totalImages) {
+                        break;
+                    }
+                    thumbOutput += `${startTime.format('HH:mm:ss.SSS')} --> ${endTime.format('HH:mm:ss.SSS')}\n`;
+
+                    thumbOutput += `${newStr}-${k + 1 < 10 ? '0' : ''}${k + 1}.jpg#xywh=${j * width},${
+                        i * height
+                    },${width},${height}\n\n`;
+
+                    startTime.add(interval, 'seconds');
+                    endTime.add(interval, 'seconds');
+                }
+            }
+        }
+        fs.writeFileSync(`${filename}.vtt`, thumbOutput);
+        console.log('\x1b[32m%s\x1b[0m', `${filename} Processing complete`);
         fs.access(`${filename}-01.jpg`, (err) => {
             if (!err) {
                 return;
             }
-            const width = 320;
-            const height = 180;
-            const interval = 1;
-            const col = 15;
-            const row = 15;
-            let thumbOutput = 'WEBVTT\n\n';
-            const startTime = moment('00:00:00', 'HH:mm:ss.SSS');
-            const endTime = moment('00:00:00', 'HH:mm:ss.SSS').add(interval, 'seconds');
-            const totalImages = Math.floor(duration / interval); // Total no of thumbnails
-            const totalSpirits = Math.ceil(duration / interval / (row * col)); // Total no of spirits
-            let newStr = filename.replace(/^\/app\/public/, "");
-            for (let k = 0; k < totalSpirits; k++) {
-                for (let i = 0; i < row; i++) {
-                    for (let j = 0; j < col; j++) {
-                        const currentImageCount = k * row * col + i * col + j;
-                        if (currentImageCount > totalImages) {
-                            break;
-                        }
-                        thumbOutput += `${startTime.format('HH:mm:ss.SSS')} --> ${endTime.format('HH:mm:ss.SSS')}\n`;
-
-                        thumbOutput += `${newStr}-${k + 1 < 10 ? '0' : ''}${k + 1}.jpg#xywh=${j * width},${
-                            i * height
-                        },${width},${height}\n\n`;
-
-                        startTime.add(interval, 'seconds');
-                        endTime.add(interval, 'seconds');
-                    }
-                }
-            }
-            fs.writeFileSync(`${filename}.vtt`, thumbOutput);
-            console.log('\x1b[32m%s\x1b[0m', `${filename} Processing complete`);
             const ffmpeg = spawn('ffmpeg', ['-i', `${filename}`, '-vf', 'fps=1,scale=320:180,tile=15x15', '-n', `${filename}-%02d.jpg`]);
             ffmpeg.stdout.on('data', (data) => {
                 console.log(`stdout: ${data}`);
